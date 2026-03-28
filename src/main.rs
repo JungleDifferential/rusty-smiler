9b5bc10db7ef017f3d444725b35a8711f84a795d
Darius <youremaleadrezz@gmail.com>
2026-03-28 16:36:14 -0700

    add VOICE_STATE_UPDATE intent

--- src/main.rs
+++ src/main.rs
@@ -29,15 +29,13 @@ impl EventHandler for Handler {
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().expect("Failed to load .env file");
-    // Configure the client with your Discord bot token in the environment.
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
-    // Set gateway intents, which decides what events the bot will be notified about
+    
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
+        | GatewayIntents::MESSAGE_CONTENT
+        | GatewayIntents::VOICE_STATE_UPDATE;
 
-    // Create a new instance of the Client, logging in as a bot. This will automatically prepend
-    // your bot token with "Bot ", which is a requirement by Discord for bot users.
     let mut client =
         Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
 
